018c397e9aa7510edaea83d5b8d76df8c2b2bbf8
Nomta <nomta@mail.ru>
2018-07-23 21:08:37 +0300

    Update audio-track.js

--- src/audio-player/audio-track.js
+++ src/audio-player/audio-track.js
@@ -1,25 +1,25 @@
 'use strict';
 
 var helpers 		= require('./helpers'),
-	createElement = helpers.createElement;
+    createElement 	= helpers.createElement;
 
 function AudioTrack(options) {
 	
-	var className 			= options.className,
-		buttonText 			  = options.buttonText,
-		getChildClassName = options.getChildClassName;
+	var className 		= options.className,
+	    buttonText 		= options.buttonText,
+	    getChildClassName   = options.getChildClassName;
 		
 	var initButtonText	= Array.isArray(buttonText) && buttonText.length === 2 ? buttonText[0] : '';
 		
 	var render = function(trackName) {
 		var track, playButton, trackTitle, progressBar, current;
 		
-		track 		  = createElement('div', className);
+		track 		= createElement('div', className);
 		playButton 	= createElement('button', getChildClassName('play-button'), initButtonText);
 		trackTitle 	= createElement('p', getChildClassName('track-title'), trackName);
 		
-		progressBar = createElement('div', getChildClassName('progress-bar'));
-		current 	  = createElement('div', getChildClassName('progress-bar__current'));
+		progressBar 	= createElement('div', getChildClassName('progress-bar'));
+		current 	= createElement('div', getChildClassName('progress-bar__current'));
 		
 		progressBar.appendChild(current);
 		
