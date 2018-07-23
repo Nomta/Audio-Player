3002b7ddabedb62d00fd85bddc758671b5639618
Nomta <nomta@mail.ru>
2018-07-23 21:12:41 +0300

    Update player-setter.js

--- src/audio-player/player-setter.js
+++ src/audio-player/player-setter.js
@@ -4,19 +4,19 @@ var trackState = require('./track-state');
 	
 var playerSetter = function(options) {
 	
-	this.tracks			  = options.tracks		|| [],	
+	this.tracks		= options.tracks		|| [],	
 	this.className		= options.className		|| '',
 	this.buttonText		= 'buttonText' in options ? 
-						options.buttonText 		: ['Play', 'Pause'],
+					options.buttonText : ['Play', 'Pause'],
 	this.container		= options.container		|| document.body;
 
 	var pathName	= options.pathName	|| '',
-      ext			  = options.ext			  || '.mp3';
+      ext		= options.ext	 	|| '.mp3';
 		
 	window.currentTrack = '';
 		
-	var getChildClassName  	= addClass.bind(this, '__'),
-		getModifierClassName 	= addClass.bind(this, '--');
+	var getChildClassName 		= addClass.bind(this, '__'),
+	    getModifierClassName 	= addClass.bind(this, '--');
 	
 	function addClass(separator, subClassName) {
 		if (this.className) return this.className + separator + subClassName;
@@ -27,10 +27,10 @@ var playerSetter = function(options) {
 		return pathName + trackName + ext;
 	}
 
-	this.getModifierClassName = getModifierClassName,
+	this.getModifierClassName 	= getModifierClassName,
 	this.getChildClassName  	= getChildClassName,
-	this.changeTrackState		  = trackState(this),
-	this.getUrl 				      = getUrl;
+	this.changeTrackState		= trackState(this),
+	this.getUrl 			= getUrl;
 }
 
 module.exports = playerSetter;
